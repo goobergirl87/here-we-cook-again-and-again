3fbd1172190c059f44681e6fe08080694724a664
goobergirl87 <isabelheidekorn@gmail.com>
2024-06-06 13:40:35 +0200

    Select input kind

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const mainSection = document.querySelector("main");
 const form = document.querySelector(".form");
 const inputTitle = document.querySelector("#title");
 const inputTime = document.querySelector("#time");
+const inputKind = document.querySelector("#kind");
